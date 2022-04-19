562d66c93fe569b1e13b9000585bcd04ef6086bd
adrianjdst <92326082+adrianjdst@users.noreply.github.com>
2022-04-19 17:18:55 +0100

    Delete Ficha04 directory

--- Ficha04/app.js
+++
@@ -1,9 +0,0 @@
-var person = {name:"adrian", age:"20", gender:"masculine"};
-var str = JSON.stringify(person);
-console.log(str);
-
-
-var novastr= '{"liga" :"Espanhola","epoca" : "2021-2022", "equipas" : "20"}';
-var jsonliga = JSON.parse(novastr);
-console.log(jsonliga);
-
